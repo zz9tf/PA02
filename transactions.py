253b3634225d1b9784265d4a0d02cd6282a53fce
zixin <jsczzzx@yahoo.com>
2022-03-22 13:23:25 -0400

    Zixin02

--- transactions.py
+++ transactions.py
@@ -2,7 +2,7 @@
 transaction.py is a Object Relational Mapping to the transaction table
 
 The ORM will work map SQL rows with the schema
-    (item_num,amount,category,date,description)
+    (rowid, item, amount, category, date, description)
 to Python Dictionaries.
 
 This app will store the data in a SQLite database ~/tracker.db
@@ -13,8 +13,9 @@ This app will store the data in a SQLite database ~/tracker.db
 import sqlite3
 
 def to_trans_dict(trans_tuple):
-    ''' trans is a transaction tuple (item_num, amount, category, date, description) ----- Written by Zheng'''
+    ''' trans is a transaction tuple (rowid, item, amount, category, date, description) ----- Written by Zheng, Zixin'''
     trans = {
+        'rowid':trans_tuple[0],
         'item':trans_tuple[1]
         , 'amount':trans_tuple[2]
         , 'category':trans_tuple[3]
@@ -36,6 +37,16 @@ class Transaction:
         con.commit()
         con.close()
         self.dbfile = filename
+    
+    def drop(self):
+        ''' drop old transactions table and create a new one, this is for testing ----- Written by Zixin'''
+        con= sqlite3.connect(self.dbfile)
+        cur = con.cursor()
+        cur.execute('''drop table if exists transactions''')
+        cur.execute('''CREATE TABLE transactions
+                    (item text, amount int, category text, date int, description text)''')
+        con.commit()
+        con.close()
 
     def select_all(self):
         ''' return all of the transactions as a list of dicts. ----- Written by Zheng'''
@@ -79,12 +90,12 @@ class Transaction:
 
         return last_rowid[0]
 
-    def delete(self, item):
-        ''' delete a transaction defined by item # ----- Written by Zixin '''
+    def delete(self, rowid):
+        ''' delete a transaction defined by rowid ----- Written by Zixin '''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute('''delete from transactions
-                        where item = (?)''',(item,))
+                        where rowid = (?)''',(rowid,))
         con.commit()
         con.close()
     
@@ -96,8 +107,4 @@ class Transaction:
         tuples = cur.fetchall()
         con.commit()
         con.close()
-        res = {
-            'date':tuples[0][0],
-            'sum':tuples[0][1]
-        }
-        return res
+        return tuples
