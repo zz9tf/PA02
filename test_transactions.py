f385e6e3795bd0f6dbae5ef48101378d84ee34fd
GodDamnGitHub <39892107+GodDamnGitHub@users.noreply.github.com>
2022-03-23 13:56:19 -0400

    Update test_transactions.py

--- test_transactions.py
+++ test_transactions.py
@@ -12,9 +12,9 @@ def tran_db(tmpdir):
     db = Transaction(filename)
     
     for i in range(10):
-        s = str(i)
+        s = str(1+i%3)
         cat ={'item': i,
-            'amount': i*3838.92%10000,
+            'amount': (1+i%3)*100,
             'category':'category'+s,
             'date': "20220316",
             'description':'description '+s,}
@@ -61,4 +61,14 @@ def test_sumByDate_transaction(tran_db):
     date = tuples[0][0]
     sum = tuples[0][1]
     assert date == 20220316
-    assert sum == 42751.4
+    assert sum == 1900
+
+@pytest.mark.sumByCat_transaction
+def test_sumByCat_transaction(tran_db):
+    tuples = tran_db.sumByCat()
+    assert tuples[0][0] == 'category1'
+    assert tuples[0][1] == 400
+    assert tuples[1][0] == 'category2'
+    assert tuples[1][1] == 600   
+    assert tuples[2][0] == 'category3'
+    assert tuples[2][1] == 900
